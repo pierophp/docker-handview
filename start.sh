9497ac94249577d020869bc34af7839f2b2f128e
Piero Giusti <pierophp@gmail.com>
2015-04-30 15:45:36 -0300

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
+# docker-machine create --driver vmwarefusion --vmwarefusion-boot2docker-url http://localhost/boot2docker-1.6.0-vmw.iso dockervmware
 echo "`boot2docker ip` handview" | sudo tee -a  /etc/hosts
-docker run -d -p 2222:22 -p 80:80 -v /Applications/XAMPP/htdocs/handview:/var/www/handview --name handview --hostname handview pierophp/handview
+docker run -d -p 2222:22 -p 80:80 -v /var/www/handview:/var/www/handview --name handview --hostname handview pierophp/handview
