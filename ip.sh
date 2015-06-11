47caed29d7b95e1782e451b0f1b28c9f19bec373
Piero Giusti <pierophp@gmail.com>
2015-06-11 14:23:01 -0300

    Fix ip.sh

--- ip.sh
+++ ip.sh
@@ -1,1 +1,1 @@
-VBoxManage guestproperty get "ubuntu-docker" "/VirtualBox/GuestInfo/Net/0/V4/IP"
+VBoxManage guestproperty get "ubuntu-docker" "/VirtualBox/GuestInfo/Net/0/V4/IP" | awk -F " " '{print $2}'
