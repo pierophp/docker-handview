398c15b73679f073c95fb75df1e96e5ca3026ec7
Piero Giusti <pierophp@gmail.com>
2015-06-11 16:44:07 -0300

    Fix start docker

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
-eval "$(docker-machine env)"
+# eval "$(docker-machine env)"
 docker run -d \
      -p 2222:22 \
      -p 80:80 \
      -p 3306:3306 \
-     -v /mnt/hgfs/www/handview:/var/www/handview \
+     -v /var/www/handview:/var/www/handview \
      --name handview \
      --hostname handview \
      pierophp/handview
